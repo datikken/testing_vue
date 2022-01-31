21948d901f98af1cc25daf99013d82e01c0971ed
nshadybekov <nshadybekov@adsterra.com>
2022-01-31 19:00:33 +0300

    testing component methods

--- specs/github.spec.js
+++ specs/github.spec.js
@@ -2,5 +2,27 @@ import GithubCard from '@/github-card'
 import { mount } from '@vue/test-utils'
 
 describe('methods', () => {
+    test('composeUrl', () => {
+        const { composeUrl } = GithubCard.methods;
+        const expectedUrl = `https://api.github.com/users/123`;
+        expect(composeUrl(123)).toBe(expectedUrl)
+    });
 
-})
+    test('fetchData', () => {
+        const testData = 'GITHUB DATA';
+        const jsonMock = jest.fn().mockResolvedValue(testData)
+        window.fetch = jest.fn().mockResolvedValue({
+            json: jsonMock
+        });
+
+        const wrapper = mount(GithubCard, {
+            methods: {
+                composeUrl: () => `url`
+            }
+        });
+
+        expect(window.fetch).toHaveBeenCalledWith('url')
+        expect(jsonMock).toHaveBeenCalled()
+        expect(wrapper.vm.data).toBe(testData)
+    })
+});
