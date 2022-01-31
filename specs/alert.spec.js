5e8fa833c110d88f86ac489740b0acb504cf643f
nshadybekov <nshadybekov@adsterra.com>
2022-01-31 19:22:01 +0300

    test lifecycle methods

--- specs/alert.spec.js
+++ specs/alert.spec.js
@@ -1,2 +1,26 @@
 import AlertMessage from '@/alert-message'
 import { mount } from '@vue/test-utils'
+
+
+describe('lifecycle methods', () => {
+   test('mounted assigns interval', () => {
+       const wrapper = mount(AlertMessage)
+       expect(wrapper.vm.interval).not.toBe(undefined)
+   });
+
+   test('counter works', () => {
+       jest.useFakeTimers()
+       const wrapper = mount(AlertMessage);
+       expect(wrapper.vm.counter).toBe(0);
+       jest.advanceTimersByTime(1000);
+       expect(wrapper.vm.counter).toBe(1);
+   });
+
+    test('instance gets destoyed', () => {
+        const beforeDestroyedSpy = jest.spyOn(AlertMessage, 'beforeDestroy')
+        const wrapper = mount(AlertMessage)
+        wrapper.vm.counter = wrapper.vm.timer - 1;
+        jest.advanceTimersByTime(1000);
+        expect(beforeDestroyedSpy).toHaveBeenCalled()
+    });
+});
