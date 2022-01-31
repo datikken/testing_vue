39c4602e9f58d5c343c24747e73b234552f9b5ea
nshadybekov <nshadybekov@adsterra.com>
2022-01-31 17:32:53 +0300

    shallow mount example

--- specs/test.spec.js
+++ specs/test.spec.js
@@ -1,7 +1,18 @@
 import TestComponent from '@/test';
-import { mount } from '@vue/test-utils';
+import { mount, shallowMount } from '@vue/test-utils';
+import List from "../src/list";
 
-test('first-spec', () => {
-    const wrap = mount(TestComponent)
-    console.log(wrap)
-})
+test('mount vue component', () => {
+    const wrap = mount(TestComponent, {
+      propsData: {
+          value: 'vue-yes'
+      }
+    })
+
+    expect(wrap.html()).toMatchSnapshot()
+});
+
+test(`List component shallow`, () => {
+    console.log(mount(List).html())
+    console.log(shallowMount(List).html())
+});
