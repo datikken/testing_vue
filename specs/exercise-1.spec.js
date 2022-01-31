0d4d518e4d985bbc78bfc564038e582223da433b
nshadybekov <nshadybekov@adsterra.com>
2022-01-31 21:54:06 +0300

    faker render list

--- specs/exercise-1.spec.js
+++ specs/exercise-1.spec.js
@@ -1,2 +1,15 @@
 import UserList from '@/exercise-1';
 import { mount } from '@vue/test-utils';
+import { faker } from '@faker-js/faker';
+
+test('component renders the users', () => {
+    const wrapper = mount(UserList, {
+        propsData: {
+            users: [ faker.name.findName() ]
+        }
+    });
+
+    const li = wrapper.find('li');
+
+    expect(li.text()).toBe(wrapper.props('users')[0]);
+})
